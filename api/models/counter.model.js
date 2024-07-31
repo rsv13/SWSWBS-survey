bbf93745171d31ae9f155a6fc3f3583242edb7e7
rsv13 <rsv130393@gmail.com>
2024-07-31 02:46:43 +0100

    Create User and distinct username model

---
+++ api/models/counter.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+// Counter Schema and Model
+const counterSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  count: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Counter = mongoose.model("Counter", counterSchema);
+
+export default Counter;
---
+++ api/models/user.model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import Counter from "./counter.js";
+
+// User Schema and Model
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    ageGroup: {
+      type: String,
+      required: true,
+      enum: ["16 to 24", "25 to 34", "35 to 44", "45 to 54", "54 and above"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female", "Prefer not to say", "other"], // Optional: enum to limit gender values
+    },
+  },
+  { timestamps: true }
+);
+
+// Pre-save hook to generate the username
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isNew) {
+    return next();
+  }
+
+  try {
+    const counter = await Counter.findOneAndUpdate(
+      { name: "usernameCounter" },
+      { $inc: { count: 1 } },
+      { new: true, upsert: true }
+    );
+
+    user.username = `SWSWBS${counter.count}`;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
