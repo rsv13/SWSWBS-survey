cd8eb1ab06563adc6193db61c71b90ef333c0132
rsv13 <rsv130393@gmail.com>
2024-07-31 02:30:07 +0100

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on 3000");
-});
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on 3000");
+    });
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
